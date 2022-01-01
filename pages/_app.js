e73ed765703157b8a54bf9488fd7f33d6b1248fd
Yuvaraj <yuvaraj1392002@gmail.com>
2022-01-01 20:37:46 +0530

    Initial Commit with MetaMask login and logout

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
 import '../styles/globals.css'
+import { MoralisProvider } from "react-moralis"
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <MoralisProvider appId={process.env.NEXT_PUBLIC_API_ID}
+      serverUrl={process.env.NEXT_PUBLIC_SERVER_URL}
+    >
+
+      <Component {...pageProps} />
+    </MoralisProvider>
+  )
 }
 
 export default MyApp
